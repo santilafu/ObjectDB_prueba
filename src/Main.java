9f9ff8fb3d0d449e3f2e7ccf47e1448b8ec4f541
santilafu <santi10dy@gmail.com>
2025-05-21 12:37:56 +0200

    V2.0

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,31 @@ import javax.persistence.*;
 
 public class Main {
     public static void main(String[] args) {
+        // Creamos el gestor de entidades a partir de nuestro archivo persistence.xml
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("miBD");
         EntityManager em = emf.createEntityManager();
-        System.out.println("Conexión a ObjectDB exitosa");
 
+        // Creamos una transacción
+        em.getTransaction().begin();
+
+        // Creamos personas y las guardamos en la base de datos
+        Persona p1 = new Persona("Juan", 25);
+        Persona p2 = new Persona("Ana", 30);
+        Persona p3 = new Persona("Luis", 35);
+
+        // Guardamos las personas en la base de datos
+        em.persist(p1);
+        em.persist(p2);
+        em.persist(p3);
+
+        // Confirmamos la transacción (guardar de verdad en la base de datos)
+        em.getTransaction().commit();
+
+        //Cerramos el gestor de entidades
         em.close();
         emf.close();
+
+        // Mostramos que las personas han dio guardadas en la base de datos.
+        System.out.println("Personas guardadas en la base de datos:");
     }
 }
