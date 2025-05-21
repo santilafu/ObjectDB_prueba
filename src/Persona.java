b6e1bb3b2869e6903312e9eec9994e30e00ef556
santilafu <santi10dy@gmail.com>
2025-05-21 12:24:05 +0200

    V1.1

--- src/Persona.java
+++ src/Persona.java
@@ -1,3 +1,53 @@
 import java.util.*;
 import javax.persistence.*;// Necesario para las anotaciones JPA
 import java.io.Serializable;
+
+/**
+ * Clase Persona que representa a una entidad de la base de datos.
+ * Va a ser guardada como un objeto completo dentro de ObjectDB.
+ */
+@Entity //Marca la clase como una entidad que se guardara en la base de datos
+public class Persona implements Serializable{
+    @Id // Indica que este atributo es la clave primaria
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Genera el valor automáticamente
+    private int id;
+    private String nombre;
+    private int edad;
+
+    // Constructor por defecto
+    public Persona() {
+    }
+    // Constructor con parámetros
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+    // Getters y Setters obligatorios para que ObjectDB pueda acceder a los atributos
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    // Método toString para mostrar la información de la persona
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", edad=" + edad +
+                '}';
+    }
+}
